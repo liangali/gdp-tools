d8fa73cacff7ed999d71991ff5b90fb9e66fec6a
liangali <liang.a.li@intel.com>
2021-09-11 19:06:15 +0800

    remove tmp.py

--- source/parse/tmp.py
+++
@@ -1,9 +0,0 @@
-from bitarray import bitarray
-
-from bitstring import BitArray, BitStream
-a = BitArray(uintle=0xaa551e57, length=32)
-print(a.hex, a.bin, a[0:4])
-
-print(a[0:4], a[4:8], a[8:16].int, a[16:32])
-
-print('done')
